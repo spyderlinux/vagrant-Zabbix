734cf0fa06b2d927d78fa1a390b296f9db4253ac
yongli.dang <yongli.dang@msfunds.com.cn>
2016-02-17 14:44:22 +0800

    Add some ruby scripts to study ruby

---
+++ ruby-scripts/01-test.rb
@@ -0,0 +1,17 @@
+class Greater
+  def initialize(name="world")
+    @name=name
+  end
+
+  def say_hi
+    puts "helo #{@name}"
+  end
+
+  def say_bye
+    puts "bye #{@name}"
+  end
+end
+
+g=Greater.new("pat")
+g.say_hi()
+g.say_bye()
